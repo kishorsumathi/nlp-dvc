b8c5c96a221183d4119a2149a6bbd06b24bac1da
kishor <kishorbrindha18@gmail.com>
2021-11-06 09:13:36 +0530

    setup.py and requiremnts.txt file modefied

--- setup.py
+++ setup.py
@@ -2,23 +2,25 @@ from setuptools import setup
 
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    LIST_OF_REQUIREMENT= f.read()
 
 ## edit below variables as per your requirements -
-REPO_NAME = "REPO_NAME"
-AUTHOR_USER_NAME = "c17hawke"
+REPO_NAME = "nlp-dvc"
+AUTHOR_USER_NAME = "kishorsumathi"
 SRC_REPO = "src"
-LIST_OF_REQUIREMENTS = []
+LIST_OF_REQUIREMENTS = [LIST_OF_REQUIREMENT]
 
 
 setup(
     name=SRC_REPO,
     version="0.0.1",
     author=AUTHOR_USER_NAME,
-    description="A small package for DVC",
+    description="simple nlp usecase",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    author_email="sunny.c17hawke@gmail.com",
+    author_email="kishorbrindha18@gmail.com",
     packages=[SRC_REPO],
     license="MIT",
     python_requires=">=3.6",
